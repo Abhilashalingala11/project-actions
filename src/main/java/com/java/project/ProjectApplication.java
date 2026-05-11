dbad84ddddbc8b7d15085ebd6601d0e0aa3e9e93
Abhilasha <lingalaabhilasha2020@gmail.com>
2026-05-11 18:19:52 +0530

    code

---
+++ src/main/java/com/java/project/ProjectApplication.java
@@ -0,0 +1,20 @@
+package com.java.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ProjectApplication {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome to project";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectApplication.class, args);
+    }
+
+}
